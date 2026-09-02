d14520b1437183d296debc8a38847ddc17861e37
Ali1234537 <aliimraniswebdev1@gmail.com>
2026-09-02 11:51:39 -0700

    Update dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,6 +4,8 @@ import { getCurrentUser } from "@/lib/auth";
 
 import DashboardClient from "./DashboardClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function DashboardPage() {
   const user = await getCurrentUser();
 
